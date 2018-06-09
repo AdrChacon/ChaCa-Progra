1358e693a8e18b177b781aea64ba73c5d6af5a96
Adr-Chaca <36579925+AdrChacon@users.noreply.github.com>
2018-06-08 22:11:16 -0600

    jnkjkj

--- Intro y Taller Progra- TEC/Iteracion/tarea9-intro-achacon.py
+++ Intro y Taller Progra- TEC/Iteracion/tarea9-intro-achacon.py
@@ -1,7 +1,7 @@
 class Suma:
     def Sumaringresados(self):
         result = 0
-        ValIngresado = 0
+##        ValIngresado = 0
         NoCero = True
         while NoCero:
             ValIngresado = int(input())
